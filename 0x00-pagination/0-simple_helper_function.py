fa53bf25efe5c87ae4f7b8706a48dbb800016443
BM-Daniel <danbabereyir@gmail.com>
2023-09-18 14:27:58 +0000

    Task 1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,8 +5,10 @@ Write a function named index_range that takes two integer arguments page and
 page_size
 '''
 
+from typing import Tuple
 
-def index_range(page, page_size):
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''Index range function'''
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 '''
-Copy index_range from the previous task and the following class into
-your code
+Implement a method named get_page that takes two integer arguments page with
+default value 1 and page_size with default value 10.
 '''
 
 import csv
 import math
-from typing import List
+from typing import List, Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     '''Index range function'''
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
@@ -38,9 +38,16 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Use assert to verify that both arguments are integers greater than 0.
+        Use index_range to find the correct indexes to paginate the dataset
+        correctly and return the appropriate page of the dataset.
+        If the input arguments are out of range for the dataset, an empty
+        list should be returned.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
-        index = index_range(page, page_size)
-        dataset = self.dataset()
+        index: Tuple = index_range(page, page_size)
+        dataset: List = self.dataset()
         return dataset[index[0]: index[1]]
